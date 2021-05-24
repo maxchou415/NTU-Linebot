f3207ff68c4061c25f395b69c4e8e418ba59c6fd
Max Chou <yihsunmaxchou@icloud.com>
2021-05-25 06:39:39 +0800

    Add dynamic function name call support

---
+++ utils/functionMapping.js
@@ -0,0 +1,7 @@
+module.exports = {
+  WHAT_TO_EAT: 'whatToEat',
+  LOST_STUDENT_ID: 'lostStudentId',
+  CONTRIBUTORS: 'listContributors',
+  SECRET_TELEPORT_POINT: 'teleportPoint',
+  MINECRAFT_NTU: 'minecraftNTU'
+}
