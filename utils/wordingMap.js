decf6d2695ffe494e3779bd63f8f9b5f591ead85
Max Chou <yihsunmaxchou@icloud.com>
2021-05-25 06:38:53 +0800

    Add common command mapping file

---
+++ utils/wordingMap.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // As known as ActionMap
+  line: {
+    incoming: {
+      WHAT_TO_EAT: '臺大附近美食',
+      LOST_STUDENT_ID: '學生證掛失',
+      MINECRAFT_NTU: '線上臺大校園 Minecraft NTU',
+      CONTRIBUTORS: '作者猴群'
+    }
+  }
+}
