6acf50113d28facbf895b36a547c77dd0ae67032
Max Chou <yihsunmaxchou@icloud.com>
2021-05-26 16:16:02 +0800

    Add findNearbyRestaurantsNotice

--- utils/wordingMap.js
+++ utils/wordingMap.js
@@ -5,7 +5,8 @@ module.exports = {
       WHAT_TO_EAT: '臺大附近美食',
       LOST_STUDENT_ID: '學生證掛失',
       MINECRAFT_NTU: '線上臺大校園 Minecraft NTU',
-      CONTRIBUTORS: '作者猴群'
+      CONTRIBUTORS: '作者猴群',
+      FIND_NEARBY_RESTAURANTS: '吃吃吃'
     }
   }
 }
